1e9fd408eed552cb2aec831769579c6c5b9c8250
maria.brito <maria.brito@processor.com.br>
2022-02-07 08:33:46 -0300

    melhorias na tela de chat

--- pages/chat.js
+++ pages/chat.js
@@ -1,5 +1,187 @@
-export default function PaginaDoChat() {
-    return(
-        <div>Página do Chat</div>
+import { Box, Text, TextField, Image, Button } from '@skynexui/components';
+import React from 'react';
+import appConfig from '../config.json';
+
+export default function ChatPage() {
+    const [message, setMessage] = React.useState('');
+    const [listOfMessages, setListOfMessages] = React.useState([]);
+
+
+    function handleNewMessage(newMessage) {
+        const messageObject = {
+            id: listOfMessages.length + 1,
+            whoSent: 'mariaedutt',
+            textMessage: newMessage,
+        };
+
+        setListOfMessages([
+            messageObject,
+            ...listOfMessages
+        ]);
+        setMessage('');
+    }
+    
+    return (
+        <Box
+            styleSheet={{
+                display: 'flex', alignItems: 'center', justifyContent: 'center',
+                //backgroundColor: appConfig.theme.colors.primary[500],
+                backgroundImage: 'url(https://i.pinimg.com/564x/07/7e/aa/077eaa5764747812c023de31ebc49f47.jpg)',
+                backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
+                color: appConfig.theme.colors.neutrals['000']
+            }}
+        >
+            <Box
+                styleSheet={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    flex: 1,
+                    boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+                    borderRadius: '5px',
+                    backgroundColor: appConfig.theme.colors.neutrals[700],
+                    height: '100%',
+                    maxWidth: '95%',
+                    maxHeight: '95vh',
+                    padding: '32px',
+                }}
+            >
+                <Header />
+                <Box
+                    styleSheet={{
+                        position: 'relative',
+                        display: 'flex',
+                        flex: 1,
+                        height: '80%',
+                        backgroundColor: appConfig.theme.colors.neutrals[600],
+                        flexDirection: 'column',
+                        borderRadius: '5px',
+                        padding: '16px',
+                    }}
+                >
+
+                    <MessageList mensagens={listOfMessages} /> 
+                    {listOfMessages}
+                    <Box
+                        as="form"
+                        styleSheet={{
+                            display: 'flex',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <TextField
+                            value={message}
+                            placeholder="Insira sua mensagem aqui..."
+                            type="textarea"
+                            styleSheet={{
+                                width: '100%',
+                                border: '0',
+                                resize: 'none',
+                                borderRadius: '5px',
+                                padding: '6px 8px',
+                                backgroundColor: appConfig.theme.colors.neutrals[800],
+                                marginRight: '12px',
+                                color: appConfig.theme.colors.neutrals[200],
+                            }}
+                            onChange={(event) => {
+                                const valueOfMessage = event.target.value;
+                                setMessage(valueOfMessage);
+                            }}
+
+                            onKeyPress={(event) => {
+                                //Se evento receber a tecla enter, então vai fazer...
+                                if (event.key === 'Enter') {
+                                    //Remove o comportamento padrão do enter
+                                    event.preventDefault();
+                                    handleNewMessage(message);
+
+                                };
+                            }}
+                        />
+                    </Box>
+                </Box>
+            </Box>
+        </Box>
+    )
+}
+
+function Header() {
+    return (
+        <>
+            <Box styleSheet={{ width: '100%', marginBottom: '16px', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }} >
+                <Text variant='heading5'>
+                    Chat
+                </Text>
+                <Button
+                    variant='tertiary'
+                    colorVariant='neutral'
+                    label='Logout'
+                    href="/"
+                />
+            </Box>
+        </>
+    )
+}
+
+function MessageList(props) {
+    console.log('MessageList', props);
+    return (
+        <Box
+            tag="ul"
+            styleSheet={{
+                overflow: 'scroll',
+                display: 'flex',
+                flexDirection: 'column-reverse',
+                flex: 1,
+                color: appConfig.theme.colors.neutrals["000"],
+                marginBottom: '16px',
+            }}
+        >
+            {props.mensagens.map((mensagem) => {
+            <Text
+                key={mensagem.id}
+                tag="li"
+                styleSheet={{
+                    borderRadius: '5px',
+                    padding: '6px',
+                    marginBottom: '12px',
+                    hover: {
+                        backgroundColor: appConfig.theme.colors.neutrals[700],
+                    }
+                }}
+            >
+                <Box
+                    styleSheet={{
+                        marginBottom: '8px',
+                    }}
+                >
+                    <Image
+                        styleSheet={{
+                            width: '20px',
+                            height: '20px',
+                            borderRadius: '50%',
+                            display: 'inline-block',
+                            marginRight: '8px',
+                        }}
+                        src={`https://github.com/vanessametonini.png`}
+                    />
+                    <Text tag="strong">
+                        {mensagem.de}
+                    </Text>
+                    <Text
+                        styleSheet={{
+                            fontSize: '10px',
+                            marginLeft: '8px',
+                            color: appConfig.theme.colors.neutrals[300],
+                        }}
+                        tag="span"
+                    >
+                        {(new Date().toLocaleDateString())}
+                    </Text>
+                </Box>
+                {mensagem.texto}
+            </Text>
+            })}
+        </Box>
+        
     );
 }
